0af9ce7181938fa51a14084e2d19e9ba6620c502
Amar281996 <91563339+Amar281996@users.noreply.github.com>
2026-03-15 20:56:57 +0530

    running the script from workflow

---
+++ prime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for num in {1..10}
+do
+    if [ $num -lt 2 ]; then
+        continue
+    fi
+
+    is_prime=1
+
+    for ((i=2; i<=num/2; i++))
+    do
+        if [ $((num%i)) -eq 0 ]; then
+            is_prime=0
+            break
+        fi
+    done
+
+    if [ $is_prime -eq 1 ]; then
+        echo $num
+    fi
+done
